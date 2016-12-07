9981d2d6afa0ed70eb103c01b69cf41cda5b9ac9
Iftikhar <mailtoiftikhar@gmail.com>
2016-12-07 13:07:51 +0500

    I have done it first time

---
+++ Assertions/src/myClass.java
@@ -0,0 +1,13 @@
+//import java.io.Console;
+
+public class myClass {
+	
+	public static void Main(String [] args)
+	{
+		System.out.print("This is is the program");
+		
+		//assert true: "Hello";
+		assert false: "You are not wellcome";
+	}
+
+}
